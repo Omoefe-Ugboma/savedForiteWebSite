c2c2afd53c0bc3564874e69a48aafcc7fe6fe785
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-06-29 13:18:27 -0700

    Update on the bookmark app, added to local storage

--- js/main.js
+++ js/main.js
@@ -9,8 +9,55 @@ function saveBookmark(e){
         name: siteName,
         url: siteUrl
     }
+    // Test if bookmarks is nul
+    if(localStorage.getItem('bookmarks') == null){
+        var bookmarks = [];
+        // add to array
+        bookmarks.push(bookmark);
+        // set to localstorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }else{
+        // get bookmark from localstorage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // Add bookmark to array
+        bookmarks.push(bookmark);
+        // Re-set back to storage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        
+    }
+    
+    // Local Storage Test
+    // localStorage.setItem('test','Hello World');
+    // console.log(localStorage.getItem('test'));
+    // localStorage.removeItem('test');
+    // console.log(localStorage.getItem('test'));
+    // console.log(bookmark);
+     
+     fetchBookmarks();
 
-    console.log(bookmark);
-
+    // Prevent form from submitting 
     e.preventDefault();
+}
+
+function fetchBookmarks(){
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    var bookmarksResults = document.getElementById('bookmarksResults');
+    
+    //   build output
+    bookmarksResults.innerHTML = '';
+    for(var i = 0; i < bookmarks.length; i++){
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class="well">'+
+                                      '<h3>'+name+'</h3>'
+                                      '</div>';
+
+
+
+
+    }
+    // console.log(bookmarks);
 }
