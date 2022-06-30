858f2dae21b9b6687c14361a5adc86377971ec12
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-06-30 21:21:18 +0100

    Bookmark app completed

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ function saveBookmark(e){
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
 
+   if(!validateForm(siteName,siteUrl)){
+    return false;
+   }
+
     var bookmark = {
         name: siteName,
         url: siteUrl
@@ -33,6 +37,9 @@ function saveBookmark(e){
     // console.log(localStorage.getItem('test'));
     // console.log(bookmark);
      
+    // clear form
+    document.getElementById('myForm').reset();
+
      fetchBookmarks();
 
     // Prevent form from submitting 
@@ -52,7 +59,10 @@ function fetchBookmarks(){
         var url = bookmarks[i].url;
 
         bookmarksResults.innerHTML += '<div class="well">'+
-                                      '<h3>'+name+'</h3>'
+                                      '<h3>'+name+
+                                      '<a class="btn btn-default" href="'+url+'">Visit</a>'+
+                                      '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger btn-sm">Delete</a>'+
+                                      '</h3>'+
                                       '</div>';
 
 
@@ -61,3 +71,44 @@ function fetchBookmarks(){
     }
     // console.log(bookmarks);
 }
+
+
+
+// Validate Form
+function validateForm(siteName,siteUrl){
+    if(!siteName || !siteUrl){
+        alert('Please fill in the form');
+        return false;
+    }
+
+    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if(!siteUrl.match(regex)){
+        alert('Please use a valid URL');
+        return false;
+    }
+
+    return true;
+}
+
+function deleteBookmark(url){
+    // Get bookMark 
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    // Loop through bookmarks
+    for(var i = 0; i <bookmarks.length; i++){
+        if(bookmarks[i].url == url){
+            // Remove from array
+            bookmarks.splice(i, 1);
+        }
+    }
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+
+    // re-fetch bookmarks
+    fetchBookmarks();
+
+}
+
+
